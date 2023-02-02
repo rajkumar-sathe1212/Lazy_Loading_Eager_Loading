e3689374b3c0643b7eba2047d501ddae00b9d77c
Rajkumar Sathe <satheraj1400@gmail.com>
2023-02-02 11:56:10 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EagerModule } from './eager/eager.module';
+import { HomeComponent } from './home/home.component';
+import { LazyModule } from './lazy/lazy.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EagerModule,
+    LazyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
